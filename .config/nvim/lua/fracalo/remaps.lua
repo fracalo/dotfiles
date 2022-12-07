927daaf75add211e4add7b34432eea902d618b34
Francesco Calo <fcaloisme at gmail.com>
2022-12-07 16:45:32 +0100

    Adding some mappings

--- .config/nvim/lua/fracalo/remaps.lua
+++ .config/nvim/lua/fracalo/remaps.lua
@@ -57,7 +57,6 @@ nnoremap("<leader>(", ":nohlsearch<cr><cr>")
 -- imap <M-BS> <esc>vBc
 -- imap <C-P> <up>
 -- imap <C-N> <down>
-
 local telescopeBuiltIn = require("telescope.builtin")
 nnoremap("<C-W>", function()
 	telescopeBuiltIn.marks()
@@ -74,3 +73,9 @@ end);
 
 -- vim.keymap.set('n', 'nt', '<cmd>Telescope file_browser<cr>', {buffer = 0, noremap=true})
 nnoremap("<C-N>", "<cmd>Telescope file_browser<cr>")
+
+nnoremap("<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>")
+
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", {silent = true })
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", {silent = true })
+
