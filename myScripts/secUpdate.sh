153ebb7181561205d29d553e02890da4300852d5
Francesco Calò <fcaloisme@gmail.com>
2026-05-10 19:22:24 +0200

    Update .gitignore

--- myScripts/secUpdate.sh
+++ myScripts/secUpdate.sh
@@ -15,9 +15,9 @@ cidr=$ip/32
 
 echo cidr is $cidr
 
-aws ec2 authorize-security-group-ingress  \
-  --group-id $GroupId  \
-  --protocol tcp --port $port --cidr $cidr
+aws ec2 authorize-security-group-ingress \
+  --group-id "$GroupId" \
+  --protocol tcp --port "$port" --cidr "$cidr"
 
 #aws ec2 modify-security-group-rules  \
 #  --group-id $GroupId --security-group-rules \
